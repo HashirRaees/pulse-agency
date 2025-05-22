cee7965b040a3ff9b16458cae79134e86dfb6cbc
HashirRaees <hashirrais13@gmail.com>
2025-05-22 23:14:51 +0500

    fixed issue

--- src/components/landing_page_components/cro_page_components/VideoSection.tsx
+++ src/components/landing_page_components/cro_page_components/VideoSection.tsx
@@ -7,7 +7,7 @@ export default function VideoSection(){
              <div className="md:w-[1200px] w-full h-[80px] md:h-[50px] flex items-center justify-center">
                 <h1 className="md:text-5xl text-4xl font-bold">How Pulse Works</h1>
             </div>
-            <iframe className="md:h-[600px] h-[400px] w-[90%] md:w-[80%]" width="560" height="315" src="https://www.youtube.com/embed/k2maqlyUuVw?si=8AzRyHI2-A9ehiC0"  title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+            <iframe className="md:h-[600px] h-[400px] w-[90%] md:w-[80%]" width="560" height="315" src="https://www.youtube.com/embed/k2maqlyUuVw?si=8AzRyHI2-A9ehiC0"  title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share">
             </iframe>
         </main>
         </>
